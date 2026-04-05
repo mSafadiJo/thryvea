8106a2e4e3e9d8b73703e44300929e7b1d8e1a97
mSafadiJo <m.alsafadi.jo@gmail.com>
2026-04-05 17:21:24 +0300

    Update CrmApi.php

--- app/Services/CrmApi.php
+++ app/Services/CrmApi.php
@@ -16,7 +16,7 @@ class CrmApi {
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 20,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $method,
