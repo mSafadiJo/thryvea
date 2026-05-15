2b77c04528d7dbc7b9d921afdc43908ba5cdcfbb
mSafadiJo <m.alsafadi.jo@gmail.com>
2026-05-16 01:19:54 +0300

    edit lead detales

--- resources/views/Admin/CampaignLeads/ReceviedLeadDetails.blade.php
+++ resources/views/Admin/CampaignLeads/ReceviedLeadDetails.blade.php
@@ -1684,12 +1684,11 @@
                         </div>
                     </div>
                     @php
-                        //$response_ping_arr = \App\Models\CrmResponsePing::where('ping_id', $campaignLeads->lead_ping_id)->get();
-                        //if($response_ping_arr->isEmpty()){
+                        $response_ping_arr = \App\Models\CrmResponsePing::where('ping_id', $campaignLeads->lead_ping_id)->get();
+                        echo "<pre>"; print_r($response_ping_arr);
+                        if($response_ping_arr->isEmpty()){
                           $response_ping_arr = \App\Models\CrmResponsePing::where('lead_id', $campaignLeads->lead_id)->get();
-                        //}
-
-                        echo "<pre>" ;print_r($response_ping_arr);
+                        }
                         $response_ping = "";
                         $url_input_ping = "";
                         if( !empty($response_ping_arr) ){
