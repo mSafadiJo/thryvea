c9345064972149621b6bb8393bc9cd51337c7674
mSafadiJo <m.alsafadi.jo@gmail.com>
2026-05-21 00:45:15 +0300

    added ion solar

--- app/Services/Allied/PostCRMAllied.php
+++ app/Services/Allied/PostCRMAllied.php
@@ -1730,7 +1730,7 @@ class PostCRMAllied {
                 //Ion Solar
                 $Lead_data_array['lead_source_ion'] = "thv$google_ts";
                 $Lead_data_array['original_source'] = "thv$google_ts";
-                $Lead_data_array['price'] = $Lead_Cost;
+                $Lead_data_array['price'] = 50;
                 $Lead_data_array['state_ion'] = $statename_code;
 
                 switch ($crm_details['service_id']){
@@ -1753,7 +1753,7 @@ class PostCRMAllied {
 
         if( config('app.env', 'local') == "local" || !empty($data_msg['is_test']) ) {
             //Test Mode
-            $Lead_data_array['is_test'] = true;
+            //$Lead_data_array['is_test'] = true;
             //$Lead_data_array['first_name'] = "SPWRTest";
         }
 
